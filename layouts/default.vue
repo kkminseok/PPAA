12950cb8b94fd20eea80db3d0fab17fb2b9f96df
kms <aaaa727@khu.ac.kr>
2023-07-24 17:33:20 +0900

    feat: 짜잘한것들 삭제, 검색창 추가, 지역 테이블 추가중..

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,37 @@
       :clipped="clipped"
       fixed
       app
+      width="30%"
     >
+    <v-toolbar
+      dense
+      floating
+      width="100%"
+    >
+      <v-text-field
+          hide-details
+          prepend-icon="mdi-magnify"
+          single-line
+          label="지역을 검색하세요."
+          clearable
+        ></v-text-field>
+    </v-toolbar>
+    <v-row
+        v-for="k in 3"
+        :key="k"
+        align="start"
+        no-gutters
+      >
+        <v-col
+          v-for="n in 3"
+          :key="n"
+        >
+          <v-sheet class="pa-2 ma-2">
+            .align-start
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <!--
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -23,6 +53,7 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      -->
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
@@ -50,40 +81,17 @@
       </v-btn>
       <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; {{ new Date().getFullYear() }} KMS</span>
     </v-footer>
   </v-app>
 </template>
@@ -108,10 +116,11 @@ export default {
           to: '/inspire'
         }
       ],
+      //초기 메뉴바 축소 확대
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Price Per Area Apartment'
     }
   }
 }
